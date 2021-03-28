54c43b36f21cbfc9197c2ea6d232203116ebbe8a
vincent_dubois_efrei_net <vincent.dubois@efrei.net>
2021-03-28 11:32:50 +0200

    add guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {HomepageComponent} from "./routes/homepage/homepage.component";
 import {ArticlesComponent} from "./routes/articles/articles.component";
+import {DetailsGuard} from "./guard/details.guard";
 
 const routes: Routes = [
   { path: '', component: HomepageComponent },
-  { path: 'article', component: ArticlesComponent }
+  { path: 'article', component: ArticlesComponent, canActivate: [DetailsGuard] }
 ];
 
 @NgModule({
