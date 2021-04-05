13685fa749cb6b76895bfa43df0f4ee7cbf1efe0
nerstak <roy.karst@gmail.com>
2021-04-05 22:55:23 +0200

    Correct date format in articles

--- src/app/routes/articles/articles.component.ts
+++ src/app/routes/articles/articles.component.ts
@@ -10,6 +10,8 @@ import {ActivatedRoute} from "@angular/router";
 })
 export class ArticlesComponent implements OnInit {
   detailedArticleToDisplay : Fields | null | undefined;
+  dateStart: String | undefined;
+  dateEnd: String | undefined;
   accessibility? : boolean;
 
   constructor(private detailArticleService : DetailsArticleService, private route : ActivatedRoute) { }
@@ -17,6 +19,11 @@ export class ArticlesComponent implements OnInit {
   ngOnInit(): void {
     //Get the detailed article
     this.detailedArticleToDisplay = this.route.snapshot.data.record.fields;
+    if(this.detailedArticleToDisplay != null) {
+      this.dateStart = new Date(this.detailedArticleToDisplay.date_start).toLocaleDateString();
+      this.dateEnd = new Date(this.detailedArticleToDisplay.date_end).toLocaleDateString();
+    }
+
     //The accessibility icons will be displayed if at least one of them is present
     this.accessibility = (this.detailedArticleToDisplay?.deaf === 1) || (this.detailedArticleToDisplay?.pmr === 1) || (this.detailedArticleToDisplay?.blind === 1);
   }
